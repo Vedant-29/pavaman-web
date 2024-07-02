d91bab91c162826fbb7c241aa05bb124e618e771
Vedant-29 <agarwalvedant42@gmail.com>
2024-07-03 04:45:32 +0530

    Adding components -- employee profile page -- 4

--- src/components/DefaultSidebar.jsx
+++ src/components/DefaultSidebar.jsx
@@ -14,22 +14,61 @@ import {
   PowerIcon,
 } from "@heroicons/react/24/solid";
 import { useNavigate } from 'react-router-dom';
+import {useAuth} from "../hooks/auth";
 
 function DefaultSidebar( {collapsable = false}) {
   const [activeItem, setActiveItem] = useState('Employee list');
   const [isCollapsed, setIsCollapsed] = useState(false);
   const navigate = useNavigate();
+  const [showLogOutPopup, setShowLogOutPopup] = useState(false);
+  const { signOut } = useAuth();
+
 
   const menuItems = [
-    { name: 'Employee list', icon: PresentationChartBarIcon, onclick: "/employee-list" },
-    { name: 'Inbox', icon: ShoppingBagIcon, onclick: "/employee-list" },
-    { name: 'Profile', icon: UserCircleIcon, onclick: "/employee-list" },
-    { name: 'Settings', icon: Cog6ToothIcon, onclick: "/employee-list" },
-    { name: 'Log Out', icon: PowerIcon, onclick: "/employee-list" },
+    { name: 'Employee list', icon: PresentationChartBarIcon, onclick: () => navigate("/employee-list") },
+    { name: 'Inbox', icon: ShoppingBagIcon, onclick: () => navigate("/inbox") },
+    { name: 'Profile', icon: UserCircleIcon, onclick: () => navigate("/profile") },
+    { name: 'Settings', icon: Cog6ToothIcon, onclick: () => navigate("/settings") },
+    { name: 'Log Out', icon: PowerIcon, onclick: () => setShowLogOutPopup(true) }, // Toggle popup for logout
   ];
+
+  const handleSignOut = () => {
+    signOut();
+  }
 
   return (
     <div className="flex">
+      {showLogOutPopup && (
+        <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
+          <div className="bg-white rounded-lg shadow-lg w-96">
+            <div className="text-lg flex items-center justify-between font-medium mb-3 px-6 py-4 shadow-custom-light relative">
+              <div>Log out</div>
+              <div className="w-8 h-8 flex items-center justify-center bg-transparent text-white " onClick={() => setShowSignUpPopup(false)}>
+                <RxCross2 className="text-[#E75A5A]" />
+              </div>
+            </div>
+            <div className="flex flex-col items-center justify-center mb-4 px-6">
+              <div className="text-center">
+                Are you sure you want to log out? You will be redirected to the homepage upon logging out.
+              </div>
+            </div>
+            <div className="flex justify-end px-6 pb-6 mt-4">
+              <div
+                className="focus:outline-none w-full flex items-center justify-center bg-white px-6 px-6 lg:px-11 py-2.5 text-[#E75A5A] rounded-md cursor-pointer font-medium sm:text-xs lg:text-base"
+                onClick={() => setShowLogOutPopup(false)}
+              >
+                Cancel
+              </div>
+              <div
+                className="focus:outline-none ml-2 w-full flex items-center justify-center bg-[#E75A5A] px-6 px-6 lg:px-11 py-2.5 text-white rounded-md cursor-pointer font-medium sm:text-xs lg:text-base"
+                onClick={handleSignOut}
+              >
+                Sign out
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
       <Card className={`h-[calc(100vh-56px)] ${collapsable ? 'w-20' : 'w-60'} p-4 shadow-xl shadow-blue-gray-900/5 transition-width duration-300`}>
         <List className="space-y-2">
           {menuItems.map((item) => (
@@ -41,8 +80,10 @@ function DefaultSidebar( {collapsable = false}) {
                   : 'hover:bg-blue-100'
               }`}
               onClick={() => {
-                navigate(item.onclick);
-                setActiveItem(item.name)
+                item.onclick(); // Execute the onclick function
+                if (item.name !== 'Log Out') {
+                  setActiveItem(item.name);
+                }
               }}
             >
               <ListItemPrefix>
