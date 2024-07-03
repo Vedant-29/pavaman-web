eed8d5873337be5b4e8f5eb63174756e9f5e1442
Vedant-29 <agarwalvedant42@gmail.com>
2024-07-03 22:42:53 +0530

    Adding environment variables

--- src/config/supabase-client.js
+++ src/config/supabase-client.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = "https://efhhibhbclwpwsgowsus.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVmaGhpYmhiY2x3cHdzZ293c3VzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTkyMDc0NTEsImV4cCI6MjAzNDc4MzQ1MX0.LbHa9t_Lkyfn96c-2UtIxpJHBK-NFA01GTMtK_kV0cg";
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
