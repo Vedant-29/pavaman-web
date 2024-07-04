e69e1c3688fdf5db22430b99109e5cf5e6a1953f
Vedant-29 <agarwalvedant42@gmail.com>
2024-07-04 23:29:31 +0530

    Adding environment variables -- 4

--- src/pages/TestGoogleMaps/Intro.jsx
+++ src/pages/TestGoogleMaps/Intro.jsx
@@ -1,19 +1,41 @@
-import { APIProvider, Map } from '@vis.gl/react-google-maps';
-import React from 'react'
+import {
+  APIProvider,
+  AdvancedMarker,
+  InfoWindow,
+  Map,
+  Marker,
+  Pin,
+} from "@vis.gl/react-google-maps";
+import React, { useState } from "react";
 
 function Intro() {
+  const [open, setOpen] = useState(false);
+
   const position = {
     lat: 53.34,
-    lng: 10
+    lng: 10,
   };
 
   return (
-    <APIProvider apiKey="AIzaSyB6Osf9bMzZ9pm4DgM6yUocYzZDUf154Ao">
-      <div style={{height: "100vh"}}>
-        <Map zoom={10} center={position}></Map>
+    <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
+      <div style={{ height: "100vh", width: "100%" }}>
+        <Map
+          zoom={12}
+          center={position}
+          mapId={import.meta.env.VITE_PUBLIC_MAP_ID}
+        >
+          <AdvancedMarker position={position} onClick={() => setOpen(true)}>
+            <Pin background={"red"} borderColor={"red"} glyphColor={"white"} />
+          </AdvancedMarker>
+          {open && (
+            <InfoWindow position={position} onCloseClick={() => setOpen(false)}>
+              <p>Im in hamburg</p>
+            </InfoWindow>
+          )}
+        </Map>
       </div>
     </APIProvider>
-  )
+  );
 }
 
-export default Intro
+export default Intro;
